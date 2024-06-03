0b9ff806da2a756d93d97a91a84f4be59cd3c355
RalfiBahar <ralfibahar2005@hotmail.com>
2024-06-03 10:23:58 -0700

    feat: finish up all strategies
     - Finally add pair strategy
     - No surrender, double down
     - Includes hit, stand

--- strategies.py
+++ strategies.py
@@ -30,3 +30,16 @@ soft_hand_strategy = {
     20: {2: "stand", 3: "stand", 4: "stand", 5: "stand", 6: "stand", 7: "stand", 8: "stand", 9: "stand", 10: "stand", 11: "stand"},
     21: {2: "stand", 3: "stand", 4: "stand", 5: "stand", 6: "stand", 7: "stand", 8: "stand", 9: "stand", 10: "stand", 11: "stand"}
 }
+
+pair_strategy = {
+    4: {2: "split", 3: "split", 4: "split", 5: "split", 6: "split", 7: "split", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    6: {2: "split", 3: "split", 4: "split", 5: "split", 6: "split", 7: "split", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    8: {2: "hit", 3: "hit", 4: "hit", 5: "split", 6: "split", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    10: {2: "hit", 3: "hit", 4: "hit", 5: "hit", 6: "hit", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    12: {2: "split", 3: "split", 4: "split", 5: "split", 6: "split", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    14: {2: "split", 3: "split", 4: "split", 5: "split", 6: "split", 7: "split", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    16: {2: "split", 3: "split", 4: "split", 5: "split", 6: "split", 7: "split", 8: "split", 9: "split", 10: "split", 11: "split"},
+    18: {2: "split", 3: "split", 4: "split", 5: "split", 6: "split", 7: "stand", 8: "split", 9: "split", 10: "stand", 11: "stand"},
+    20: {2: "stand", 3: "stand", 4: "stand", 5: "stand", 6: "stand", 7: "stand", 8: "stand", 9: "stand", 10: "stand", 11: "stand"},
+    22: {2: "split", 3: "split", 4: "split", 5: "split", 6: "split", 7: "split", 8: "split", 9: "split", 10: "split", 11: "split"}
+}
