afb63b77866c1337a828a5681bb8825e933432dd
RalfiBahar <ralfibahar2005@hotmail.com>
2024-06-03 13:36:43 -0700

    feat: initialize BlackjackGame class with deck and running count

--- blackjack_game.py
+++ blackjack_game.py
@@ -5,3 +5,11 @@ from strategies import hard_hand_strategy, soft_hand_strategy, pair_strategy
 import time
 import random
 import csv
+
+class BlackjackGame:
+    def __init__(self, deck, running_count):
+        self.deck = deck
+        self.player_hand = []
+        self.dealer_hand = []
+        self.split_hands = []
+        self.running_count = running_count
