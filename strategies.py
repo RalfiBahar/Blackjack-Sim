8eaa0be7d9f4ab7a0bb10de610aaa8c4d49bd523
RalfiBahar <ralfibahar2005@hotmail.com>
2024-06-03 10:21:22 -0700

    feat: create strategies file with strategy dicts, created hard hand strategy
    - Hard Hand Strategy added

---
+++ strategies.py
@@ -0,0 +1,19 @@
+hard_hand_strategy = {
+    5: {2: "hit", 3: "hit", 4: "hit", 5: "hit", 6: "hit", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    6: {2: "hit", 3: "hit", 4: "hit", 5: "hit", 6: "hit", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    7: {2: "hit", 3: "hit", 4: "hit", 5: "hit", 6: "hit", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    8: {2: "hit", 3: "hit", 4: "hit", 5: "hit", 6: "hit", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    9: {2: "hit", 3: "hit", 4: "hit", 5: "hit", 6: "hit", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    10: {2: "hit", 3: "hit", 4: "hit", 5: "hit", 6: "hit", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    11: {2: "hit", 3: "hit", 4: "hit", 5: "hit", 6: "hit", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    12: {2: "hit", 3: "hit", 4: "stand", 5: "stand", 6: "stand", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    13: {2: "stand", 3: "stand", 4: "stand", 5: "stand", 6: "stand", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    14: {2: "stand", 3: "stand", 4: "stand", 5: "stand", 6: "stand", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    15: {2: "stand", 3: "stand", 4: "stand", 5: "stand", 6: "stand", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    16: {2: "stand", 3: "stand", 4: "stand", 5: "stand", 6: "stand", 7: "hit", 8: "hit", 9: "hit", 10: "hit", 11: "hit"},
+    17: {2: "stand", 3: "stand", 4: "stand", 5: "stand", 6: "stand", 7: "stand", 8: "stand", 9: "stand", 10: "stand", 11: "stand"},
+    18: {2: "stand", 3: "stand", 4: "stand", 5: "stand", 6: "stand", 7: "stand", 8: "stand", 9: "stand", 10: "stand", 11: "stand"},
+    19: {2: "stand", 3: "stand", 4: "stand", 5: "stand", 6: "stand", 7: "stand", 8: "stand", 9: "stand", 10: "stand", 11: "stand"},
+    20: {2: "stand", 3: "stand", 4: "stand", 5: "stand", 6: "stand", 7: "stand", 8: "stand", 9: "stand", 10: "stand", 11: "stand"},
+    21: {2: "stand", 3: "stand", 4: "stand", 5: "stand", 6: "stand", 7: "stand", 8: "stand", 9: "stand", 10: "stand", 11: "stand"}
+}
