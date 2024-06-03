178a6edefa852405bf20e574c7b140d21f737bb2
RalfiBahar <ralfibahar2005@hotmail.com>
2024-06-03 13:47:20 -0700

    feat: add start_game method (and several sample methods without implementation) to BlackjackGame class
     - start_game methods
     - several required methods without implementation
     - add gitignore (and ignore pycache)

--- blackjack_game.py
+++ blackjack_game.py
@@ -13,3 +13,38 @@ class BlackjackGame:
         self.dealer_hand = []
         self.split_hands = []
         self.running_count = running_count
+
+    def start_game(self):
+        self.player_hand = [self.deal_card(), self.deal_card()]
+        self.dealer_hand = [self.deal_card(), self.deal_card()]
+
+        if self.calculate_score(self.player_hand) == 21:
+            if self.calculate_score(self.dealer_hand) == 21:
+                return "tie"
+            return "player_blackjack"
+
+        self.play_hand(self.player_hand)
+
+        for hand in self.split_hands:
+            self.play_hand(hand)
+
+        if all(self.calculate_score(hand) > 21 for hand in [self.player_hand] + self.split_hands):
+            return "player_bust"
+
+        self.dealer_turn()
+        return self.determine_winner()
+    
+    def deal_card(self):
+        pass
+
+    def calculate_score(self):
+        pass
+
+    def play_hand(self):
+        pass
+
+    def dealer_turn(self):
+        pass
+
+    def determine_winner(self):
+        pass
