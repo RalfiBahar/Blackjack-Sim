0ddf2390f7ee67356f1e88d69c7c4cd9894f415c
RalfiBahar <ralfibahar2005@hotmail.com>
2024-06-09 07:15:19 -0700

    chore: remove unused dependencies

--- op-black-frontend/src/chartjs-plugin-downsample.d.ts
+++
@@ -1,5 +0,0 @@
-// src/types/chartjs-plugin-downsample.d.ts
-declare module "chartjs-plugin-downsample" {
-  const downsample: (data: any[], options: { threshold: number }) => any[];
-  export default downsample;
-}
